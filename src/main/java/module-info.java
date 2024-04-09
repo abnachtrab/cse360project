4e9c68dd4a75868cc1b1ab3b5d250cb605b29003
abnachtrab <abnachtrab@gmail.com>
2024-04-09 16:28:29 -0700

    Added first parts of backend

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module dev.ln13.cse360project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires java.sql;
+    opens dev.ln13.cse360project.frontend to javafx.fxml;
+    exports dev.ln13.cse360project.frontend;
+    exports dev.ln13.cse360project.backend;
+}
