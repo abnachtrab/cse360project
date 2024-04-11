b5ff8353aa6434fa85ee7eb2d6da8260a0c14d16
GHittASU <ghitt@asu.edu>
2024-04-10 23:02:45 -0700

    last minute what I could do

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module cse360project {
     requires javafx.fxml;
 
     requires java.sql;
+    requires javafx.base;
     opens dev.ln13.cse360project.frontend to javafx.fxml;
     exports dev.ln13.cse360project.frontend;
     exports dev.ln13.cse360project.backend;
